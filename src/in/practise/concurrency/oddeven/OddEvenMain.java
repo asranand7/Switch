6df4bb23d1558f43fbd15cb251b69925e118479d
Anand <asranand7@gmail.com>
2024-05-20 13:44:30 +0530

    Odd Even Printer

--- src/in/practise/concurrency/oddeven/OddEvenMain.java
+++ src/in/practise/concurrency/oddeven/OddEvenMain.java
@@ -7,47 +7,48 @@ public class OddEvenMain {
         AtomicInteger atomicInteger = new AtomicInteger(1);
         Object lock = new Object();
 
-        Runnable oddPrinter = new Runnable() {
-            @Override
-            public void run() {
-                synchronized (lock) {
-                    while (atomicInteger.get() < 20) {
-                        while (atomicInteger.get() % 2 == 0) {
-                            try {
-                                lock.wait();
-                            } catch (InterruptedException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                        System.out.println("Odd " + atomicInteger.getAndIncrement());
-                        lock.notifyAll();
-                    }
-                }
-            }
-        };
+        Printer oddPrinter = new Printer(1, 2, 20, lock, atomicInteger);
+        Printer evenPrinter = new Printer(2, 2, 20, lock, atomicInteger);
 
-        Runnable evenPrinter = new Runnable() {
-            @Override
-            public void run() {
-                synchronized (lock) {
-                    while (atomicInteger.get() <= 20) {
-                        while (atomicInteger.get() % 2 == 1) {
-                            try {
-                                lock.wait();
-                            } catch (InterruptedException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                        System.out.println("Even " + atomicInteger.getAndIncrement());
-                        lock.notifyAll();
-                    }
-                }
-            }
-        };
-
-        Thread t1 = new Thread(oddPrinter);
-        Thread t2 = new Thread(evenPrinter);
-       t2.start();
+        Thread t1 = new Thread(oddPrinter, "OddPrinter");
+        Thread t2 = new Thread(evenPrinter, "EvenPrinter");
+        t2.start();
         t1.start();
+    }
+
+     static class Printer implements Runnable{
+
+        private int currentValue;
+        private int step;
+        private Object lock;
+        private int maxValue;
+        private AtomicInteger counter;
+
+        public Printer(int startValue, int step, int maxValue, Object lock, AtomicInteger counter){
+            this.currentValue =startValue;
+            this.step = step;
+            this.maxValue = maxValue;
+            this.lock = lock;
+            this.counter = counter;
+        }
+
+        @Override
+        public void run() {
+            synchronized (lock){
+                while(counter.get() < maxValue){
+                    while(counter.get() != currentValue ){
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    System.out.println(Thread.currentThread().getName() +  " " + counter.get());
+                    currentValue = counter.get() + step;
+                    counter.getAndIncrement();
+                    lock.notifyAll();
+                }
+            }
+        }
     }
 }
